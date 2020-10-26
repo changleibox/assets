1ca0110c75dd44303d68ca83d948937733520bb7
changlei <changlei@graspyun.com>
2020-10-26 10:57:39 +0800

    first commit

---
+++ bin/assets.dart
@@ -0,0 +1,5 @@
+import 'package:assets/assets.dart' as assets;
+
+void main(List<String> arguments) {
+  print('Hello world: ${assets.calculate()}!');
+}
---
+++ test/assets_test.dart
@@ -0,0 +1,8 @@
+import 'package:assets/assets.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
