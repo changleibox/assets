527600fbf11042809042b58d064cf6c417b9dc6c
changlei <changlei@graspyun.com>
2020-10-26 17:08:52 +0800

    修改单元测试

--- test/assets_test.dart
+++ test/assets_test.dart
@@ -1,8 +1,8 @@
 import 'dart:io';
 
 import 'package:assets/assets.dart';
-import 'package:test/test.dart';
 import 'package:path/path.dart' as path;
+import 'package:test/test.dart';
 
 void main() {
   test('resolve', () {
@@ -13,7 +13,11 @@ void main() {
     for (var key in keys) {
       var subPaths = fileMap[key];
       for (var subPath in subPaths) {
-        File(path.join('assets', key, subPath)).createSync(recursive: true);
+        final file = File(path.join('assets', key, subPath));
+        if (file.existsSync()) {
+          continue;
+        }
+        file.createSync(recursive: true);
       }
     }
     resolve();
